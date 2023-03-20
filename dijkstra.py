91fba2a54ef9f4dd2341a3e77a5e98c4767ea6ca
nehamarne27 <nehamarne27@gmail.com>
2023-03-20 16:06:01 -0400

    added the video writer and color coded the path

--- dijkstra.py
+++ dijkstra.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import pprint
 import heapq as hq
-
+import math
 # global canvas
 def draw_map():
     #upper rectangle
@@ -43,10 +43,10 @@ def get_start_node(map):
     print("enter start node. entering convention should be x y where bottom left is the origin. keep space between the cordinate not comma")
     # after entering you should see the node updated on the map
     # start_node = input()
-    start_node = np.array([50, 110])
-    # start_node=[int(j) for j in input().split()]
-    # print("start node is")
-    # print(start_node)
+    start_node=[int(j) for j in input().split()]
+    start_node = tuple(start_node)
+    print("start node is")
+    print(start_node)
     valid_node=node_validity(map, start_node)
     while(not valid_node):
         print("node you entered is in obstacle space. plese reenter a valid node")
@@ -56,15 +56,16 @@ def get_start_node(map):
     # cv2.circle(map, (start_node[0], start_node[1]), 0, (0, 255, 0), -1)
     # cv2.imshow('color_map',map)
     # cv2.waitKey(0)
+    start_node = (50, 110)
     return map, start_node
 
 def get_goal_node(map):
     print("enter goal node. entering convention should be x y where bottom left is the origin. keep space between the cordinate not comma")
     # after entering you should see the node updated on the map
     # start_node = input()
-    # goal_node=[int(j) for j in input().split()]
-    # print(start_node)
-    goal_node = np.array([200, 200])
+    goal_node=[int(j) for j in input().split()]
+    goal_node = tuple(goal_node)
+    print(goal_node)
     valid_node=node_validity(map, goal_node)
     while(not valid_node):
         print("node you entered is in obstacle space. plese reenter a valid node")
@@ -73,6 +74,7 @@ def get_goal_node(map):
     map[goal_node[1], goal_node[0]] = (0, 0, 255)
 
     # cv2.circle(map, (goal_node[0], goal_node[1]), 1, (0, 0, 255), -1)
+    goal_node = (50, 200)
     return map, goal_node
 
 def node_validity(map, node):
@@ -87,162 +89,291 @@ def node_validity(map, node):
     else:
         False
 
+def h_cost_calc(node, goal_node):
+    # print(node)
+    # print(goal_node)
+    x1,y1=node[0:2]
+    x2,y2=goal_node[0:2]
+    # print(x1, y1, x2, y2)
+    dist = math.sqrt((x1-x2)**2+(y1-y2)**2)
+    return dist
+
 def explore_map(map, start_node, goal_node):
     print("inside explore")
+    output_video = cv2.VideoWriter('Proj3_neha_anukriti.avi', cv2.VideoWriter_fourcc(*'XVID'), 800, (map.shape[1], map.shape[0])) 
     # parent_node = start_node
     # cost present node parent node
     open_list = [(0, start_node, start_node)]#heap data structure of tuples 
     closed_list = {}#dict
     hq.heapify(open_list)
     # hq.heappush(open_list)
-    print(open_list)
+    # print(open_list)
     while(len(open_list)):
     # open_list.append(start_node)
         # newclosed list is a tuple of 3 element
         new_closed_list_element = hq.heappop(open_list)
-        print(new_closed_list_element)
+        # print(new_closed_list_element)
         closed_list[tuple(new_closed_list_element[1])]=tuple(new_closed_list_element[2])
-        print(closed_list)
+        # print(closed_list)
         parent_node = new_closed_list_element[1]
         parent_node_cost = new_closed_list_element[0]
+        if(h_cost_calc(parent_node, goal_node))<2:
+            final_parent_node = parent_node
+            # print(bcolors.FAIL+"Goal node has been reachead going to backtrack the path"+bcolors.ENDC)
+            back_track(start_node, final_parent_node, output_video, closed_list, map)
+            break
         # explore and update the nbr
-        open_list, map, node_up = add_node_up(open_list, closed_list, map, parent_node, parent_node_cost)
-        open_list, map, node_up = add_node_down(open_list, closed_list, map, parent_node, parent_node_cost)
-        open_list, map, node_up = add_node_right(open_list, closed_list, map, parent_node, parent_node_cost)
-        open_list, map, node_up = add_node_left(open_list, closed_list, map, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_down(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_right(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_left(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_right_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_left_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_right_bottom(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        open_list, map, node_up, output_video = add_node_left_bottom(open_list, closed_list, map, output_video, parent_node, parent_node_cost)
+        
+    return map
+
+def back_track(start_position, final_parent_node, output_video, closed_list, map):
+    # print(bcolors.FAIL+"closted list is"+str(closed_list)+bcolors.ENDC)
+    child_node = final_parent_node
+    parent_node = closed_list[child_node]
+    # print(child_node)
+    # print(parent_node)
+    # parent_node = final_parent_node
+    # child_node = closed_list[final_parent_node]
+    # cv2.circle(canvas,(int(parent_node[0]),int(parent_node[1])),2,(255,255,255),-1)
+    while(child_node != parent_node):
+        map[child_node[1], child_node[0]] = (0, 120, 120)
+        # cv2.circle(map,(int(child_node[0]),int(child_node[1])),2,(0,120,120),-1)
+        # cv2.imshow('canvas',canvas)
+        # cv2.waitKey(0)
+        child_node = parent_node
+        parent_node = closed_list[child_node]
+        # time.sleep(1)
+        map=np.uint8(map)
+        output_video.write(map)
+    # for k in closed_list:
+    #     canvas[k[1], k[0]] = [255] * 3
+    #     cv2.imshow(canvas)
+    output_video.release()
 
 # only checks if node can be added
-def add_node_up(open_list, closed_list, map, parent_node):
-    print("checking to add node up")
+def add_node_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
+    # print("checking to add node up")
     child_node = (parent_node[0], parent_node[1]+1)
-    print(child_node)
+    # print(child_node)
     # if the node is present in the obstacle space and and is not present in closed list
     if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
         # add /update elemnt in open list
-        new_cost = parent_node[0]+1
+        new_cost = parent_node_cost+1
         new_open_list_element = [new_cost, child_node, parent_node]
         hq.heappush(open_list, new_open_list_element)
         hq.heapify(open_list)
         # if (new_cost<child_node[0]):
             # new_open_list = (new_cost, child_node, parent_node)
-        cv2.imshow('map', map)
-        cv2.waitKey(0)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
         #  print("m here")
         # open_list.append(new_open_list)
-        return open_list, map, True
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
+        return open_list, map, False, output_video
 # only checks if node can be added
-def add_node_down(map, parent_node):
-    # print("checking to add node up")
-    child_node = np.array([parent_node[0], parent_node[1]-1])
-    print(child_node)
-    if(node_validity(map, child_node)):
+def add_node_down(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
+    # print("checking to add node down")
+    child_node = (parent_node[0], parent_node[1]-1)
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
+        return open_list, map, False, output_video
 # only checks if node can be added
-def add_node_right(map, parent_node):
-    # print("checking to add node up")
-    child_node = np.array([parent_node[0]+1, parent_node[1]])
-    print(child_node)
-    if(node_validity(map, child_node)):
+def add_node_right(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
+    # print("checking to add node right")
+    child_node = (parent_node[0]+1, parent_node[1])
+    # print(child_node)
+    # print("open list is"+str(open_list))
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
+        return open_list, map, False, output_video
 # only checks if node can be added
-def add_node_left(map, parent_node):
-    # print("checking to add node up")
-    child_node = np.array([parent_node[0]-1, parent_node[1]])
-    print(child_node)
-    if(node_validity(map, child_node)):
-        # print("up node can be added")
-        # add/update cost
-        # if 
-        # child_node.cost = parent_node.cost+1
-        # add/update parent
-        # child_node.parent = parent_node
-        # if the node has not been explored add it to the OL
-        # OL.append(child_node)
-        map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
-    else:
-        return False
-        # print("could not add node up")
-
-# only checks if node can be added
-def add_node_right_up(map, parent_node):
-    # print("checking to add node up")
-    child_node = np.array([parent_node[0]+1, parent_node[1]+1])
-    print(child_node)
-    if(node_validity(map, child_node)):
+def add_node_left(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
+    # print("checking to add node left")
+    child_node = (parent_node[0]-1, parent_node[1])
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
+        return open_list, map, False, output_video
 # only checks if node can be added
-def add_node_left_up(map, parent_node):
+def add_node_right_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
     # print("checking to add node up")
-    child_node = np.array([parent_node[0]-1, parent_node[1]+1])
-    print(child_node)
-    if(node_validity(map, child_node)):
+    child_node = (parent_node[0]+1, parent_node[1]+1)
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1.4
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
+        return open_list, map, False, output_video
 # only checks if node can be added
-def add_node_right_bottom(map, parent_node):
+def add_node_left_up(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
     # print("checking to add node up")
-    child_node = np.array([parent_node[0]+1, parent_node[1]-1])
-    print(child_node)
-    if(node_validity(map, child_node)):
+    child_node = (parent_node[0]-1, parent_node[1]+1)
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1.4
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
+        return open_list, map, False, output_video
 # only checks if node can be added
-
-def add_node_left_bottom(map, parent_node):
+def add_node_right_bottom(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
     # print("checking to add node up")
-    child_node = np.array([parent_node[0]-1, parent_node[1]-1])
-    print(child_node)
-    if(node_validity(map, child_node)):
+    child_node = (parent_node[0]+1, parent_node[1]-1)
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
         # print("up node can be added")
         map[child_node[1], child_node[0]] = (0, 255, 0)
-        return map, True
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1.4
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
     else:
-        return False
-        # print("could not add node up")
-
-
-
+        return open_list, map, False, output_video
+# only checks if node can be added
+def add_node_left_bottom(open_list, closed_list, map, output_video, parent_node, parent_node_cost):
+    # print("checking to add node up")
+    child_node = (parent_node[0]-1, parent_node[1]-1)
+    # print(child_node)
+    # if the node is present in the obstacle space and and is not present in closed list
+    if(node_validity(map, child_node) and not closed_list.__contains__(child_node) ):
+        # print("up node can be added")
+        map[child_node[1], child_node[0]] = (0, 255, 0)
+        # add /update elemnt in open list
+        new_cost = parent_node_cost+1.4
+        new_open_list_element = [new_cost, child_node, parent_node]
+        hq.heappush(open_list, new_open_list_element)
+        hq.heapify(open_list)
+        # if (new_cost<child_node[0]):
+            # new_open_list = (new_cost, child_node, parent_node)
+       # print("open list is"+str(open_list))
+       # cv2.imshow('map', map)
+       # cv2.waitKey(0)
+        #  print("m here")
+        # open_list.append(new_open_list)
+        map=np.uint8(map)
+        output_video.write(map)
+        return open_list, map, True, output_video
+    else:
+        return open_list, map, False, output_video
 
 map = draw_map()
 # cv2.imshow('map', map)
 map, start_node = get_start_node(map)
 # cv2.imshow('map', map)
 map, goal_node = get_goal_node(map)
-explore_map(map, start_node, goal_node)
+map = explore_map(map, start_node, goal_node)
 cv2.imshow('map', map)
 
 cv2.waitKey(0)
